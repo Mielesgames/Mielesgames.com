2271a3489f0cc38940fcdc38ea863bdd0b9f79cf
Mielesgames <emieltlam11@gmail.com>
2023-11-22 12:09:42 +0100

    22/11/2023: 12:09:15. Build: 2

---
+++ client/src/app/Models/buildData.ts
@@ -0,0 +1,12 @@
+export const BuildData: any = {
+        "BuildNumber": 2,
+        "Branch": "main",
+        "BuildedCorrectly": true,
+        "BuilderVersion": 8,
+        "JsonVersion": 2,
+        "Builder": "Mielesgames",
+        "BuildDate": "22/11/2023: 12:09:15",
+        "LastCommitId": "33976f817aee0b2fa70ac3ad878e029bf68089f6",
+        "LastCommitURL": "https://github.com/oldmartijntje/playtime/commit/33976f817aee0b2fa70ac3ad878e029bf68089f6"
+    };
+        
