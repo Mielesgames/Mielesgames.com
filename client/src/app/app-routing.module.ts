4124b63e918d7dd2fc60bc1014e38da986271fb5
Mielesgames <emieltlam11@gmail.com>
2023-11-22 12:00:31 +0100

    move

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomepageComponent } from './Pages/homepage/homepage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'Home', component: HomepageComponent },
+  { path: '**', component: HomepageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
